65edcf81a6224c844c6b39e9699ea9726385b101
Vihaan <vickey.verma21@gmail.com>
2016-08-24 16:02:03 +0530

    added goback functionality

--- app/src/main/java/com/example/webviewtest/MainActivity.java
+++ app/src/main/java/com/example/webviewtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.webviewtest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
@@ -63,4 +64,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
 }
